119f03c468dc9ba122faa72e6aee72ec52e9c9f5
Coutrinlu20 <amarallua20@gmail.com.br>
2024-10-19 16:38:58 -0300

    acript

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+
+
 def index(request):
-    return render(request,'galeria/index.html')
+    dados = {
+        1:{"nome": " Nebuloso de Carina",
+        "legenda": "webbtelecope.org / NASA / James Webb"},
+        2:{"nome": "Galáxia NGC 1079",
+        "legenda": "nassa.org / NASA / Hunnle"}
+   }
+    return render(request,'galeria/index.html', {"cards": dados})
 
 
 def imagem(request):
