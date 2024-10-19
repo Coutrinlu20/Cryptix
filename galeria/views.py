d16dec28b88c211b774580552764065f9bb3015d
Coutrinlu20 <amarallua20@gmail.com.br>
2024-10-19 18:07:50 -0300

    script atualizado

--- galeria/views.py
+++ galeria/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     dados = {
-        1:{"nome": " Nebuloso de Carina",
+        1:{"nome": "Nebuloso de Carina",
         "legenda": "webbtelecope.org / NASA / James Webb"},
         2:{"nome": "Galáxia NGC 1079",
         "legenda": "nassa.org / NASA / Hunnle"}
